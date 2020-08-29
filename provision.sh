772a73f28c3e28abea57052eca7f8b0fd801a0c1
Faisal-97 <faisalabulnaja99@gmail.com>
2020-08-29 19:42:16 +0300

    .sh updated

--- provision.sh
+++ provision.sh
@@ -5,7 +5,7 @@ curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
 echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 sudo apt update -y
 sleep 10
-sudo apt install -y curl vim jq git make apt-transport-https helm
+sudo apt install -y curl vim jq git make apt-transport-https helm nodejs npm yarn
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 sudo curl -sL get.docker.com | sh
 sudo usermod -aG docker ubuntu
@@ -24,16 +24,6 @@ git clone https://github.com/hashivim/vim-terraform.git ~/.vim/pack/plugins/star
 git clone https://github.com/hashivim/vim-packer.git ~/.vim/pack/plugins/start/vim-packer
 git clone https://github.com/tpope/vim-fugitive.git ~/.vim/pack/plugins/start/fugitive
 git clone https://github.com/martinda/Jenkinsfile-vim-syntax.git ~/.vim/pack/plugins/start/jenkinsfile
-curl -sfL https://get.k3s.io | sh -
-sudo systemctl enable k3s
-sudo systemctl start k3s
-sleep 5
-sudo cat /var/lib/rancher/k3s/server/token
-sudo cp /var/lib/rancher/k3s/server/token /home/ubuntu/token
-sudo chmod a+rw /home/ubuntu/token
-sudo chown ubuntu:ubuntu /home/ubuntu/token
-sudo chmod a+r /etc/rancher/k3s/k3s.yaml
-kubectl taint node $(hostname) k3s-controlplane=true:NoSchedule
 sudo apt autoremove -y
 echo "source /home/ubuntu/.bash_aliases"
 echo 'alias k=kubectl' > /home/ubuntu/.bash_aliases
@@ -46,6 +36,5 @@ EOF
 
 echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 
-git clone https://github.com/KnowledgeHut-AWS/k8s-jenkins
 
 echo "done"
